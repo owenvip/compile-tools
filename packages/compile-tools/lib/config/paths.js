0785f1ee0f2d84795fc21a1f7a5a2a1fd7ca0b1e
owenwong <owen.cq.cn@gmail.com>
2021-06-10 16:49:00 +0800

    chore: update webpack entry

--- packages/compile-tools/lib/config/paths.js
+++ packages/compile-tools/lib/config/paths.js
@@ -4,7 +4,7 @@ const workDir = process.cwd();
 const distDir = path.resolve(workDir, "dist");
 const staticDir = path.resolve(workDir, "static");
 const tplFile = path.resolve(workDir, "index.html");
-const entry = path.resolve(workDir, "src/index.tsx");
+const entry = path.resolve(workDir, "src/index");
 
 module.exports = {
   workDir,
