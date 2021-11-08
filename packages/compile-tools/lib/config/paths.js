14e8cfe24027b117d6bbabf9c9eaf6f0c2a7241e
owenwong <owen.cq.cn@gmail.com>
2021-11-08 18:38:48 +0800

    chore: rename files

--- packages/compile-tools/lib/config/paths.js
+++ packages/compile-tools/lib/config/paths.js
@@ -1,3 +1,9 @@
+/*
+ * @Descripttion: 
+ * @Author: OwenWong
+ * @Email: owen.cq.cn@gmail.com
+ * @Date: 2021-06-03 21:41:28
+ */
 const path = require("path");
 
 const workDir = process.cwd();
@@ -5,6 +11,7 @@ const distDir = path.resolve(workDir, "dist");
 const staticDir = path.resolve(workDir, "static");
 const tplFile = path.resolve(workDir, "index.html");
 const entry = path.resolve(workDir, "src/index");
+const pkgPath = path.resolve(workDir, "package.json");
 
 module.exports = {
   workDir,
@@ -12,4 +19,5 @@ module.exports = {
   staticDir,
   tplFile,
   entry,
+  pkgPath,
 };
--- packages/compile-tools/lib/index.js
+++ packages/compile-tools/lib/index.js
@@ -1,4 +1,10 @@
-module.exports = {
-  dev: require("./config/webpack.dev.js"),
-  prod: require("./config/webpack.prod.js"),
-};
+/*
+ * @Descripttion:
+ * @Author: OwenWong
+ * @Email: owen.cq.cn@gmail.com
+ * @Date: 2021-06-03 21:41:28
+ */
+module.exports =
+  process.env.NODE_ENV === "production"
+    ? require("./config/prod")
+    : require("./config/dev");
